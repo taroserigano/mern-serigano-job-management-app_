64b691e7b6da6504b6220193371029cef09c8a92
Taro <taroserigano@gmail.com>
2023-02-11 15:48:20 -0600

    commit

---
+++ client/src/components/Logo.js
@@ -0,0 +1,7 @@
+import logo from '../assets/images/serigano.png'
+
+const Logo = () => {
+  return <img src={logo} alt='jobify' className='logo' />
+}
+
+export default Logo
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,42 @@
+import main from '../assets/images/jobb.jpg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import { useAppContext } from '../context/appContext';
+import React from 'react';
+
+const Landing = () => {
+  const { user } = useAppContext();
+  return (
+    <React.Fragment>
+      {user && <Navigate to='/' />}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className='container page'>
+          {/* info */}
+          <div className='info'>
+            <h1>
+              Recruitment <span>management</span> 
+            </h1>
+            <p>
+            The comprehensive and uttermost job management application to help you organize your 
+            hiring process nicely and neatly. 
+            Job hunting, job seeking, or job searching is the act of looking for employment, 
+            due to unemployment, underemployment, discontent with a current position, or a 
+            desire for a better position.
+            </p>
+            <Link to='/register' className='btn btn-hero'>
+              Login/Register
+            </Link>
+          </div>
+          <img src={main} alt='job hunt' className='img main-img' />
+        </div>
+      </Wrapper>
+    </React.Fragment>
+  );
+};
+
+export default Landing;
