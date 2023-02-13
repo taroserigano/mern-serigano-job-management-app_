66157e7c4d7597314df98159b006229dc705bc15
Taro Serigano <taroserigano@gmail.com>
2023-02-13 11:19:12 -0600

    Update attachCookie.js

--- utils/attachCookie.js
+++ utils/attachCookie.js
@@ -2,9 +2,9 @@ const attachCookie = ({ res, token }) => {
   const oneDay = 1000 * 60 * 60 * 24;
 
   res.cookie('token', token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
-    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true,     // only web browser can handle this 
+    expires: new Date(Date.now() + oneDay),    
+    secure: process.env.NODE_ENV === 'production',    // only HTTPS can use this cookie 
   });
 };
 
